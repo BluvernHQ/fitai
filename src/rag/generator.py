ab3ae1d4826a19f6e73da291a5e6fdefa27a7526
MonihaPS <monihaparasuraman@gmail.com>
2026-01-08 15:06:16 +0530

    Modified

--- src/rag/generator.py
+++ src/rag/generator.py
@@ -3,7 +3,7 @@ from typing import List, Dict, Any
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
